c3d9d23153ad0f2848f403d80605bcfb8e4621b1
Murad Mustafayev <murad.s.mustafayev@gmail.com>
2016-05-16 00:22:52 +0400

    my changes

--- scripts/app.js
+++ scripts/app.js
@@ -5,4 +5,52 @@ $(function(){
 			this.body.toggleClass('dark');
 		},this));
 	}();
+
+
+
+	/*****  Ticket Module  *****/
+	var ticketModule = (function() {
+		var ticket = $('.ticket');
+		var minSymbolLength = 113;
+
+		/* Show message box */
+		var showMessage = function(text) {
+			var ticketCopy = ticket.clone(); // create new ticket
+
+			// if text length is less than minimum then run with a default template text
+			if(checkMinLength(text)) {
+				ticketCopy.find('.ticket-rightSide p').text(text);
+			}
+
+			// insert ticket to DOM and animate
+			var a = ticket.before(ticketCopy.delay(1).queue(function() {
+    			ticketCopy.addClass('ticket-move').dequeue();
+    		}));
+		}
+
+		/* Check length of symbols in text */
+		var checkMinLength = function(text) {
+			return (text.length >= minSymbolLength) ? true : false;
+		}
+
+		return {
+			showMessage: showMessage
+		}
+
+	})();
+
+	/*****  End of Ticket Module  *****/
+
+	
+	window.app.getInputValue = new function(prototype) {
+		this.button = document.getElementsByTagName('button');
+		this.input 	= document.getElementById('MessageInput');
+
+		$(this.button).on('click', $.proxy(function() {
+			var inputText = this.input.value;
+
+			ticketModule.showMessage(inputText);
+		}, this));
+	}();
+
 });
